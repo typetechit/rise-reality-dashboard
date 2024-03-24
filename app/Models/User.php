873451b297eae7e9f23a9f36fc1698d8d591709e
Saifur Rahman <saifur.dohs@gmail.com>
2024-03-24 19:44:24 +0600

    Properties and blog api updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,7 @@ class User extends Authenticatable
         'location',
         'practice_area',
         'phone',
+        'social_links'
     ];
 
     /**
@@ -52,6 +53,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'social_links' => 'array'
         ];
     }
 
